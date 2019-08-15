50de42f2bec13a9bb9bbc04e5914af03c5208752
GuardianGH <zhling2012@live.com>
2019-08-15 12:10:48 +0800

    升级版本为 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sxclzy",
-    version="0.1.0",
+    version="0.1.1",
     author="GuardianAngel",
     author_email="zhling2012@live.com",
     description="a tool to schedule your functions",
